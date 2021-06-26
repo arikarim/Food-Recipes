b8a93996404af755489ebaa9390cfdd302a9384a
arikarim <arikarim146@gmail.com>
2021-06-26 13:48:24 +0300

    Add Router link for foods

--- src/Components/Foods.js
+++ src/Components/Foods.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Foods = ({ foods }) => (
   <div className="d-flex flex-wrap justify-content-between">
     {foods && foods.map((food) => (
-      <Link key={`/${food.idMeal}`} to={foods.idMeal}>
+      <Link key={food.idMeal} to={`/${foods.idMeal}`}>
         <div className="card my-2" style={{ width: '18rem' }}>
           <img src={food.strMealThumb} alt={food.strMeal} />
           <div className="card-body">
--- src/Components/FoodsDetail.js
+++ src/Components/FoodsDetail.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import { updateFoods } from '../Actions/Foods';
-// import PropTypes from 'prop-types';
 
-const FoodsDetail = () => (
-  <div />
-);
+const FoodsDetail = ({ food, updateFoods }) => {
+  const fetch = async () => {
+    const { data } = axios.get(`www.themealdb.com/api/json/v1/1/lookup.php?i=${food.idMeal}`);
+    const result = data.meals;
+    console.log(result);
+    updateFoods(food.idMeal, {
+      description: result.strDescription,
+    });
+  };
 
-// FoodsDetail.propTypes = {
+  useEffect(() => {
+    fetch();
+  }, []);
+  return (
+    <div>
+      Details
+    </div>
+  );
+};
 
-// };
+FoodsDetail.propTypes = {
+  food: PropTypes.objectOf(PropTypes.object).isRequired,
+  updateFoods: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state, props) => ({
   food: state.foods.find((food) => food.idMeal === props.match.params.id),
