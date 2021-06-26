9454590b973249ca841c835f6b09e1e953a58ae0
arikarim <arikarim146@gmail.com>
2021-06-26 14:37:48 +0300

    Add Uniq rout for each food

--- src/Components/FoodsDetail.js
+++ src/Components/FoodsDetail.js
@@ -1,36 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { updateFoods } from '../Actions/Foods';
 
-const FoodsDetail = ({ food, updateFoods }) => {
+const FoodsDetail = ({ food }) => {
+  const [foodDetail, setFoodDetail] = useState({});
   const fetch = async () => {
-    const { data } = axios.get(`www.themealdb.com/api/json/v1/1/lookup.php?i=${food.idMeal}`);
-    const result = data.meals;
-    console.log(result);
-    updateFoods(food.idMeal, {
-      description: result.strDescription,
-    });
+    try {
+      const { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${food.idMeal}`);
+      // updateFoods(food.idMeal, {
+      //   description: result.strDescription,
+      // });
+      setFoodDetail(data.meals[0]);
+      console.log(foodDetail);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
     fetch();
   }, []);
   return (
-    <div>
-      Details
+    <div className="container">
+      <div className="card my-2" style={{ width: '70vw' }}>
+        <img src={food.strMealThumb} alt={food.strMeal} />
+        <div className="card-body">
+          <h3>{food.strMeal}</h3>
+          <p className="card-text">{food.strArea}</p>
+          Instructions:
+          <p className="card-text">{food.strInstructions}</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 FoodsDetail.propTypes = {
   food: PropTypes.objectOf(PropTypes.object).isRequired,
-  updateFoods: PropTypes.func.isRequired,
+  // updateFoods: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state, props) => ({
-  food: state.foods.find((food) => food.idMeal === props.match.params.id),
+  food: state.foods.find((f) => f.idMeal === props.match.params.id),
 });
 
 const mapDispatchToProps = (dispatch) => ({
